97875b5ed70c874a2603282f8775466c1198849d
Amit Paudel <amitpaudel789@gmail.com>
2026-01-03 14:51:21 +0545

    never type

--- src/moreTypes.ts
+++ src/moreTypes.ts
@@ -9,3 +9,24 @@ let bookString='{"name":"who moved my cheese"}';
 let bookObject=JSON.parse(bookString) as Book 
 
 console.log(bookObject.name);
+
+//Never
+
+type Role='admin'|'user';
+
+function redirectRoleBased(role:Role):void{
+  if(role==='admin'){
+    console.log('Redirecting to admin dashboard')
+    return
+  }
+
+  if(role==='user'){
+    console.log('Redirecting to the user dashboard')
+    return
+  }
+  role //Never
+}
+
+function neverReturn():never{
+  while(true){}
+}
