bf5436a2077f9f22fcb67caf93b706c18f7b2a50
Amit Paudel <amitpaudel789@gmail.com>
2026-01-09 19:04:21 +0545

    react with typescript

---
+++ reactts/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import ChaiCard from './components/ChaiCard'
+
+function App() {
+ 
+
+  return (
+    <div>
+      hello
+      <ChaiCard name="Headphone" price={5000}></ChaiCard>
+    </div>
+  )
+}
+
+export default App
---
+++ reactts/src/components/ChaiCard.tsx
@@ -0,0 +1,17 @@
+interface ChaiCard{
+  name:string;
+  price:number;
+  isSpecial?:boolean
+}
+
+const ChaiCard = ({name,price,isSpecial=false}:ChaiCard) => {
+  return (
+    <div>
+      <h2>
+       The price of  {name} is {price} {isSpecial && <span>⭐</span>}
+      </h2>
+    </div>
+  )
+}
+
+export default ChaiCard
